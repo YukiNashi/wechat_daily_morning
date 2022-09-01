1858c00bfbff86aa985c4868b601e7281dee44cb
YukiNashi <112452558+YukiNashi@users.noreply.github.com>
2022-09-01 19:40:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 
 from datetime import date, datetime
 import math
+
 from wechatpy import WeChatClient
+
 from wechatpy.client.api import WeChatMessage, WeChatTemplate
 import requests
 import os
 import random
+
+from apscheduler.schedulers.blocking import BlockingScheduler
 
 today = datetime.now()
 start_date = os.environ['START_DATE']
@@ -18,8 +22,20 @@ app_secret = os.environ["APP_SECRET"]
 user_id = os.environ["USER_ID"]
 template_id = os.environ["TEMPLATE_ID"]
 
+sched = BlockingScheduler()
+
+def cron(event):
+    '''
+    设置定时任务：周一至周日14：20执行任务
+    '''
+    sched = BlockingScheduler()
+    sched.add_job(event, 'cron',  day_of_week='1-7', hour=14, minute=20)
+    sched.start()
 
 def get_weather():
+    '''
+    调用百度天气API，获取天气、温度、地点
+    '''
   url = "https://api.map.baidu.com/weather_abroad/v1/?data_type=all&ak=4TqFCTbN37fk0AXA5g2i4Suao9rODaAC&district_id=" + city
   res = requests.get(url).json()
   weather = res['result']['now']
@@ -27,22 +43,34 @@ def get_weather():
   return weather['text'], math.floor(weather['temp']), location['city']
 
 def get_count():
+    '''
+    计算累计日期
+    '''
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
 def get_birthday():
+    '''
+    计算生日倒数日
+    '''
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
 
 def get_words():
+    '''
+    调用今日诗词API，获取一句诗词
+    '''
   words = requests.get("https://v1.jinrishici.com/tianqi/xingxing")
   if words.status_code != 200:
     return get_words()
   return words.json()['content']
 
 def get_random_color():
+    '''
+    随机颜色
+    '''
   return "#%06x" % random.randint(0, 0xFFFFFF)
 
 
