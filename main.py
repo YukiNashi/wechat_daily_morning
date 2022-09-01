00b15e13b048be8a3f958700155f4acff92ca828
YukiNashi <112452558+YukiNashi@users.noreply.github.com>
2022-09-01 19:35:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,10 +37,10 @@ def get_birthday():
   return (next - today).days
 
 def get_words():
-  words = requests.get("https://api.shadiao.pro/chp")
+  words = requests.get("https://v1.jinrishici.com/tianqi/xingxing")
   if words.status_code != 200:
     return get_words()
-  return words.json()['data']['text']
+  return words.json()['content']
 
 def get_random_color():
   return "#%06x" % random.randint(0, 0xFFFFFF)
