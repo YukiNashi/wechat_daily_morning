d301580af6c2f5b79081e234931a5de8b7829685
YukiNashi <112452558+YukiNashi@users.noreply.github.com>
2022-09-05 12:49:26 +0800

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,7 @@ def get_birthday_total():
 
 def get_birthday():
 # 计算生日倒数日
-  birthday1 = datetime.strptime(birthday, "%m-%d")
-  next = datetime.strptime(str(date.today().year) + "-" + birthday1, "%Y-%m-%d")
+  next = datetime.strptime(str(date.birthday.month) + "-" + str(date.birthday.day), "%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
